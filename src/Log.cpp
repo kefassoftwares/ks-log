16f1319a7d14dc312b5f80bd5c2e861f0ceec4f9
Bineesh N <Bineesh N>
2016-06-23 01:31:11 +0530

    made it in two files

--- src/Log.cpp
+++ src/Log.cpp
@@ -1,14 +1,7 @@
 #include <sstream>
 #include <ctime>
 
-#if defined(__unix__) || defined(__linux__) 
-#include <sys/stat.h>
-#elif defined(_WIN32)
-#include <Windows.h>
-#endif
-
 #include "Log.h"
-#include "Utils.h"
 
 namespace klog {
 
@@ -139,4 +132,84 @@ namespace klog {
         static Log log;
         return log;
     }
+
+        std::string RealtimeTokenParser::getSimpleTime() const
+    {
+        char timeArray[50];
+        std::ostringstream timeStream;
+    
+#if defined(__unix__) || defined(__linux__)
+        timeval tv;
+        gettimeofday(&tv, 0);
+        std::strftime(timeArray, sizeof(timeArray), "%Y-%b-%d %H:%M:%S.", std::localtime(&tv.tv_sec));
+        timeStream << timeArray << tv.tv_usec;
+#elif defined(_WIN32)
+        FILETIME ft;
+        SYSTEMTIME st;
+        GetSystemTimeAsFileTime(&ft);
+        FileTimeToSystemTime(&ft, &st);
+        
+        unsigned long long int t = ft.dwHighDateTime;
+        t <<= 32;
+        t |= ft.dwLowDateTime;
+
+        t /= 10;
+        t -= 11644473600000000;
+
+        std::vector<std::string> months = {"Jan", "Feb", "Mar", "Jun", "Jul", "Aug", "Sep", "Nov", "Dec"};
+        timeStream  << st.wYear << "-" << months[st.wMonth] << "-" << st.wDay 
+                    << " " << st.wHour << ":" << st.wMinute << ":" << st.wSecond << "." << t%1000000;
+#endif
+        return timeStream.str();
+
+    }
+
+    std::string RealtimeTokenParser::parse(const std::string& logMessage_, Severity logType_, const std::string& sourceFile_, long int lineNum_) const 
+    {
+        std::map<Severity, std::string> SeverityStringMap = {
+            { Severity::S_INFO, "INFO" },
+            { Severity::S_DEBUG, "DEBUG" },
+            { Severity::S_WARNING, "WARNING" },
+            { Severity::S_ERROR, "ERROR" },
+            { Severity::S_CRITICAL, "CRITICAL" }
+        };
+
+        std::ostringstream parserStream;
+        parserStream.setf(std::ios::fixed, std::ios::floatfield);
+
+        for (const Token &t : _tokenList)
+        {
+            switch (t.getType())
+            {
+            case TokenType::FILE_NAME:
+                parserStream << sourceFile_;
+                break;
+            case TokenType::LINE_NUM:
+                parserStream << lineNum_;
+                break;
+            case TokenType::MESSAGE:
+                parserStream << logMessage_;
+                break;
+            case TokenType::SEVERITY:
+                parserStream << SeverityStringMap.find(logType_)->second;
+                break;
+            case TokenType::DATE_TIME:
+                parserStream << getSimpleTime();
+                break;
+            case TokenType::THREAD_NUM:
+                parserStream << std::this_thread::get_id();
+                break;
+            default:
+                parserStream << t.getContent();
+                break;
+            }
+        }
+        std::cout << "Parser string:" << parserStream.str() << std::endl; 
+        return parserStream.str();
+    }
+
+    RealtimeTokenParser::RealtimeTokenParser(const std::vector<Token>& tokenList_):_tokenList(tokenList_)
+    {
+    }
+
 }
--- src/Log.h
+++ src/Log.h
@@ -1,15 +1,107 @@
 #pragma once
 
+#include <iostream>
 #include <fstream>
 #include <map>
 #include <thread>
 #include <sstream>
 #include <cstring>
+#include <vector>
 
-#include "Token.h"
-#include "RealtimeTokenParser.h"
+#if defined(__unix__) || defined(__linux__) 
+#include <sys/stat.h>
+#include <sys/time.h>
+#elif defined(_WIN32)
+#include <Windows.h>
+#endif
 
 namespace klog {
+
+    //the s_ is used to avoid conflict with macros in windows
+    enum class Severity
+    {
+        S_INFO = 0,
+        S_DEBUG = 1,
+        S_WARNING = 2,
+        S_ERROR = 3,
+        S_CRITICAL = 4
+    };
+
+    enum class TokenType
+    {
+        FILE_NAME = 0,
+        LINE_NUM = 1,
+        THREAD_NUM = 2,
+        DATE_TIME = 3,
+        SEVERITY = 4,
+        MESSAGE = 5,
+        STRING = 6
+    };
+
+
+    class Token
+    {
+    public:
+
+        Token(const std::string& tokenStr_)
+        {
+            if (tokenStr_.length() == 2 && tokenStr_[0] == '%')
+            {
+                findTokenType(tokenStr_[1]);
+                return;
+            }
+            
+            _type = TokenType::STRING;
+            _content = tokenStr_;
+                
+        }
+
+        TokenType getType() const { return _type; }
+        std::string getContent() const { return _content; }
+
+    private:
+
+        void findTokenType(char c)
+        {
+            switch (c)
+            {
+            case 'f':
+                _type = TokenType::FILE_NAME;
+                break;
+            case 'l':
+                _type = TokenType::LINE_NUM;
+                break;
+            case 't':
+                _type = TokenType::THREAD_NUM;
+                break;
+            case 'd':
+                _type = TokenType::DATE_TIME;
+                break;
+            case 's':
+                _type = TokenType::SEVERITY;
+                break;
+            case 'm':
+                _type = TokenType::MESSAGE;
+                break;
+            default:
+                _type = TokenType::STRING;
+                _content = std::string("%") + c;
+            }
+        }
+
+        std::string     _content;
+        TokenType       _type;
+    };
+
+    class RealtimeTokenParser
+    {
+    public:
+        RealtimeTokenParser(const std::vector<Token>& tokenList_);
+        std::string parse(const std::string& logMessage_, Severity logType_, const std::string& sourceFile_, long int lineNum_) const;
+    private:
+        const std::vector<Token> _tokenList;
+        std::string getSimpleTime() const;
+    };
 
     struct Log {
 
